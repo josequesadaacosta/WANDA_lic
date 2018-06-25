3fbd37a8251f9f56c5feadd38ee7668de736f6ba
lenapizarro <lena.pizarro@outlook.com>
2018-06-24 18:14:47 -0600

    Delete RF24forWANDA.h

--- RF24forWANDA/RF24forWANDA.h
+++
@@ -1,7 +0,0 @@
-/*
-Header file for RF24forWanda Library
-Library enables using the nRF24L01 with Teensy, to transmit audio in an ad hoc wireless network.
-*/
-
-#ifndef _RF24forWANDA_H_INCLUDED_
-#define _RF24forWANDA_H_INCLUDED_
