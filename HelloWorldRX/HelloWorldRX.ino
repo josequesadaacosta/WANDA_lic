f1d1f27c64a0681a45a1c3422b3d6c0da0831012
lenapizarro <lena.pizarro@outlook.com>
2018-07-11 19:17:16 -0600

    Add files via upload

---
+++ HelloWorldRX/HelloWorldRX.ino
@@ -0,0 +1,31 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
+RF24 radio(28, 39);
+
+void setup() {
+  const uint64_t address = 0xF0F0F0F0E1LL;
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()) {
+    char text[96] = "";
+    //char text1[32] = "";
+    //char text2[32] = "";
+    radio.read(&text, sizeof(text));
+    //radio.read(&text[32], sizeof(uint8_t)*32);
+    //radio.read(&text[64], sizeof(uint8_t)*32);
+    Serial.println("Received: ");
+    Serial.println(text);
+    //Serial.println(text1);
+    //Serial.println(text2);
+  }
+}
---
+++ HelloWorldTX/HelloWorldTX.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(28, 39);
+
+
+void setup(){
+  
+  const uint64_t address = 0xF0F0F0F0E1LL; //random byte for address. It just needs to match the address in receiver code.
+  //const byte address[6] = "00001";
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  //radio.printDetails();
+}
+
+void loop(){
+  const char text[] = "Hello World";
+  const char text1[] = "Bye World";
+  const char text2[] = "Fuck World";
+  radio.writeFast(&text, sizeof(text));
+  radio.writeFast(&text1, sizeof(text1));
+  radio.writeFast(&text2, sizeof(text2));
+  bool ok = radio.txStandBy(1000);
+  //delay();
+  //Serial.println("Sent.\n");
+}
+
